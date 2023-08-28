7191412d8e841d01cc68dd6b8c345b11850381f3
Sam-Wu-dev <sam2014521@gmail.com>
2023-08-28 14:49:32 +0800

    add modular exponentiaition

---
+++ Modular Exponentiation/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int modular_exponentiation(int b, int n, int m)
+{
+    int x = 1;
+    int power = b % m;
+    while (n > 0)
+    {
+        int current_bit = n % 2;
+        n /= 2;
+        if (current_bit == 1)
+        {
+            x = (x * power) % m;
+        }
+        power = (power * power) % m;
+    }
+
+    return x; // b^n mod m
+}
+
+int main()
+{
+    cout << modular_exponentiation(2, 3, 3) << endl;
+    cout << int(pow(2, 3)) % 3 << endl;
+}
